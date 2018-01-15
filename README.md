a044e1c86b64c48e68885c9adaf188443acaefb7
Nathan Jones <nathanj439@gmail.com>
2018-01-15 10:39:47 -0500

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a small program to generate tactics puzzles from your missed tactics on
+lichess.
+
+Running Locally
+---------------
+
+    $ ./gradlew run
---
+++ src/main/kotlin/com/github/nathanj/Main.kt
@@ -0,0 +1,168 @@
+@file:Suppress("MemberVisibilityCanPrivate")
+
+package com.github.nathanj
+
+import com.github.kittinunf.fuel.httpGet
+import com.google.gson.*
+import io.javalin.Javalin
+import io.javalin.embeddedserver.Location
+import org.alcibiade.chess.model.ChessBoardModel
+import org.alcibiade.chess.persistence.FenMarshallerImpl
+import org.alcibiade.chess.persistence.PgnMarshaller
+import org.alcibiade.chess.persistence.PgnMarshallerImpl
+import org.alcibiade.chess.rules.ChessRules
+import org.alcibiade.chess.rules.ChessRulesImpl
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import java.lang.reflect.Type
+import kotlin.math.sign
+
+data class Eval(
+        val eval: Int
+)
+
+class EvalDeserializer : JsonDeserializer<Eval> {
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Eval {
+        json.asJsonObject.get("mate")?.let {
+            return Eval(it.asInt.sign * 99999)
+        }
+        return Eval(json.asJsonObject.get("eval")!!.asInt)
+    }
+}
+
+data class Game(
+        val id: String,
+        val moves: String,
+        val color: String,
+        val analysis: List<Eval>?
+)
+
+data class LichessGames(
+        val currentPageResults: List<Game>
+)
+
+fun findBlunders(eval: List<Eval>): List<Int> {
+    val blunders = ArrayList<Int>()
+    eval.zipWithNext().forEachIndexed { index, (e1, e2) ->
+        if (Math.abs(e1.eval - e2.eval) >= 300 && Math.abs(e2.eval) > Math.abs(e1.eval)) {
+            blunders.add(index)
+        }
+    }
+    return blunders
+}
+
+fun findMissedTactics(eval: List<Eval>): List<Int> {
+    val blunders = ArrayList<Int>()
+    val threshold = 300
+    val threshold2 = threshold * 0.66
+    repeat(eval.size - 2) { i ->
+        val delta = eval[i + 1].eval - eval[i].eval
+        val delta2 = eval[i + 2].eval - eval[i + 1].eval
+        if (eval[i + 1].eval.sign == (if (i % 2 == 0) 1 else -1) &&
+                Math.abs(eval[i + 1].eval) >= threshold &&
+                Math.abs(delta) >= threshold &&
+                Math.abs(delta2) >= threshold2 &&
+                delta.sign != delta2.sign) {
+            //println("ei=${eval[i].eval} ei1=${eval[i + 1].eval} ei2=${eval[i + 2].eval} delta=$delta delta2=$delta2")
+            blunders.add(i)
+        }
+    }
+    return blunders
+}
+
+private val context = AnnotationConfigApplicationContext(ChessRulesImpl::class.java, PgnMarshallerImpl::class.java)
+private val rules = context.getBean(ChessRules::class.java)
+private val marshaller = context.getBean(PgnMarshaller::class.java)
+private val fenMarshaller = FenMarshallerImpl()
+
+fun generateBoards(games: LichessGames): Map<String, Any> {
+    val boards = ArrayList<Map<String, String>>()
+    val map = HashMap<String, Any>()
+
+    map.put("num_games", games.currentPageResults.size)
+    val analyzedGames = games.currentPageResults.filter { it.analysis != null }
+    map.put("num_analyzed_games", analyzedGames.size)
+
+    analyzedGames.forEach { game ->
+        val blunders = findMissedTactics(game.analysis!!)
+        var position = rules.initialPosition
+
+        game.moves.split(" ").forEachIndexed { i, move ->
+            val path = marshaller.convertPgnToMove(position, move)
+            val updates = rules.getUpdatesForMove(position, path)
+            val afterMove = ChessBoardModel()
+            afterMove.setPosition(position)
+            for (update in updates) {
+                update.apply(afterMove)
+            }
+            afterMove.nextPlayerTurn()
+            position = afterMove
+
+            if (blunders.contains(i - 1) &&
+                    ((game.color == "white" && i % 2 == 0) ||
+                            (game.color == "black" && i % 2 == 1))) {
+                val fen = fenMarshaller.convertPositionToString(position)
+
+                boards.add(
+                        mapOf("game_id" to game.id,
+                                "fen" to fen,
+                                "fen_link" to fen.replace(' ', '_'),
+                                "orientation" to position.nextPlayerTurn.fullName,
+                                "move_source" to path.source.pgnCoordinates,
+                                "move_destination" to path.destination.pgnCoordinates,
+                                "id" to "${game.id}_${i}"))
+            }
+        }
+
+    }
+
+    boards.shuffle()
+    map.put("boards", boards)
+    map.put("num_tactics", boards.size)
+
+    return map
+}
+
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val builder = GsonBuilder()
+        builder.registerTypeAdapter(Eval::class.java, EvalDeserializer())
+        val gson = builder.create()
+
+
+        val app = Javalin.create()
+        app.port(7000)
+        app.enableStaticFiles("static", Location.EXTERNAL)
+        app.enableStandardRequestLogging()
+        app.get("/") { ctx ->
+            ctx.renderMustache("templates/index.mustache")
+        }
+
+        app.get("/search") { ctx ->
+            val q = ctx.queryParam("q") ?: ""
+            val type = ctx.queryParam("type") ?: ""
+            if (q.isEmpty()) {
+                ctx.redirect("/")
+            } else {
+                try {
+                    val url = "https://lichess.org/api/user/$q/games?nb=50&page=1&with_analysis=1&with_moves=1"
+                    val (_, _, result) = url.httpGet().responseString()
+                    val games = gson.fromJson(result.get(), LichessGames::class.java)
+                    val data = generateBoards(games)
+                    if (type == "txt") {
+                        ctx.renderMustache("templates/search-txt.mustache", data)
+                    } else {
+                        ctx.renderMustache("templates/search.mustache", data)
+                    }
+                } catch (ex: Exception) {
+                    ctx.status(500)
+                    ctx.result("There was an error. Make sure you typed your username correctly!\nError: " + ex.toString())
+                }
+            }
+        }
+
+        app.start()
+    }
+}
